6f8cfed79f3c3700a6371e7977b8627006ad466b
vamshijai64 <155511452+vamshijai64@users.noreply.github.com>
2025-03-02 01:53:08 +0530

    chs

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
-      <Route path="/gallery" element={<Gallery />} />
+      {/* <Route path="/gallery" element={<Gallery />} /> */}
       <Route path="/product" element={<Product />} />
       <Route path="/contact" element={<Contact />} />
     </Routes>
